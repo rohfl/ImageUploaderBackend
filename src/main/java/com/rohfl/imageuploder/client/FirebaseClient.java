11247f95e9c7f024e158a41540db79376fb9b5ba
Rohit Jangid <rohfl@Rohits-MacBook-Air.local>
2022-11-11 21:19:37 +0530

    Update README.md

--- src/main/java/com/rohfl/imageuploder/client/FirebaseClient.java
+++ src/main/java/com/rohfl/imageuploder/client/FirebaseClient.java
@@ -6,7 +6,6 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.StorageClient;
 import java.io.IOException;
-import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.core.io.ClassPathResource;
@@ -26,7 +25,7 @@ public class FirebaseClient implements FileUploadClient {
     this.bucketName = bucketName;
     this.fileBaseUrl = fileBaseUrl;
     try {
-      ClassPathResource account = new ClassPathResource("gservices.json");
+      ClassPathResource account = new ClassPathResource("google_services.json");
       FirebaseOptions options =
           new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(account.getInputStream()))
               .setStorageBucket(this.bucketName).build();
